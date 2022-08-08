e211635e188284a97e821e69b50e6b6d90cf9b0a
Conrado <conradremon7@gmail.com>
2022-08-08 19:53:47 -0300

    Add steps for usage

--- src/lib/component/app/App.tsx
+++ src/lib/component/app/App.tsx
@@ -6,7 +6,9 @@ const App = () => {
     return <>
         <link rel="icon" href="svg/twitch.svg" sizes="any" type="image/svg+xml"></link>
         <title>Parallel Twitch</title>
-        <FirstSteps title="This is the first step"/>
+        <div className="stepsContainer">
+        <FirstSteps />
+        </div>
     </>
 }
 
